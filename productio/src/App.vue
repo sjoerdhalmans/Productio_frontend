d73d265861132b6d4a5f9a462f0bf69d617076dd
SKULLCRUSHER957 <sjoerd.halmans@gmail.com>
2021-04-05 16:42:43 +0200

    Began work on profile page

--- productio/src/App.vue
+++ productio/src/App.vue
@@ -16,14 +16,20 @@
 
       <div class="ml-5" v-if="!$auth.loading">
         <!-- show login when not authenticated -->
-        <v-btn v-if="$auth.isAuthenticated" @click="$router.push('profile')"
-          >Dashboard</v-btn
+        <v-btn
+          depressed
+          v-if="$auth.isAuthenticated"
+          @click="$router.push('/')"
+          >Home</v-btn
         >
       </div>
 
       <div class="ml-5" v-if="!$auth.loading">
         <!-- show login when not authenticated -->
-        <v-btn v-if="$auth.isAuthenticated" @click="$router.push('departmentoverview')"
+        <v-btn
+          depressed
+          v-if="$auth.isAuthenticated"
+          @click="$router.push('departmentoverview')"
           >Departments</v-btn
         >
       </div>
@@ -32,9 +38,35 @@
 
       <div v-if="!$auth.loading">
         <!-- show login when not authenticated -->
-        <v-btn v-if="!$auth.isAuthenticated" @click="login">Log in</v-btn>
+        <v-btn depressed v-if="!$auth.isAuthenticated" @click="login">Log in</v-btn>
         <!-- show logout when authenticated -->
-        <v-btn v-if="$auth.isAuthenticated" @click="logout">Log out</v-btn>
+        <div class="text-center" v-if="$auth.isAuthenticated">
+          <v-menu open-on-hover offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn class="menu" depressed v-bind="attrs" v-on="on">
+                menu <i class="fas fa-chevron-down"></i>
+              </v-btn>
+              <!-- <i class="fas fa-bars menu" dark v-bind="attrs" v-on="on"></i> -->
+            </template>
+            <v-list class="dropdown">
+              <v-img
+                alt="Productio Logo"
+                class="shrink mr-2 logo"
+                contain
+                src="./assets/productio.png"
+                transition="scale-transition"
+                width="30"
+              />
+              <v-list-item v-for="(item, index) in items" :key="index">
+                <v-list-item-title
+                  style="cursor: pointer"
+                  @click="item.action"
+                  >{{ item.title }}</v-list-item-title
+                >
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
       </div>
     </v-app-bar>
 
@@ -46,6 +78,20 @@
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=PT+Serif&display=swap");
+
+.v-btn {
+  color: black;
+  background-color: white !important;
+}
+
+.dropdown {
+  text-align: center;
+}
+
+.logo {
+  margin: 10px 30px;
+  display: block;
+}
 
 .blacktext {
   color: black;
@@ -59,26 +105,34 @@
 
 <script lang="ts">
 import Vue from "vue";
+import router from "./router";
 
 export default Vue.extend({
   name: "App",
 
   components: {},
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      items: [
+        {
+          title: "Profile",
+          action: () => router.push({ name: "profile" }),
+        },
+        {
+          title: "Logout",
+          action: () =>
+            this.$auth.logout({
+              returnTo: window.location.origin,
+            }),
+        },
+      ],
+    };
+  },
 
   methods: {
     login() {
       this.$auth.loginWithRedirect({});
-    },
-
-    // Log the user out
-    logout() {
-      this.$auth.logout({
-        returnTo: window.location.origin,
-      });
     },
   },
 });
--- productio/src/views/Profile.vue
+++ productio/src/views/Profile.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <div>
+  <!-- <div>
       <img :src="$auth.user.picture" />
       <h2>{{ $auth.user.name }}</h2>
       <p>{{ $auth.user.email }}</p>
@@ -9,13 +8,144 @@
 
     <div>
       <pre>{{ JSON.stringify($auth.user, null, 2) }}</pre>
-    </div>
-  </div>
+    </div> -->
+  <v-row>
+    <v-col class="content">
+      <v-row class="cardrow">
+        <v-col class="card">
+          <v-card class="card" elevation="2" tile>
+            <div class="cardcontent">
+              <h1>Welcome {{ $auth.user.nickname }} !</h1>
+              <h2 class="pt-10">Quote of the session</h2>
+              <p class="pt-5">"{{ this.randomQuote.text }}"</p>
+              <p>- {{ this.randomQuote.author }}</p>
+            </div></v-card
+          >
+        </v-col>
+      </v-row>
+      <div class="clearancecontainer pb-10">
+        <h1 style="text-align: center">Your clearance cards</h1>
+        <v-carousel class="clearancelist">
+          <v-carousel-item v-for="role in roles" :key="role">
+            <v-img
+              alt=" Logo"
+              class="shrink mr-2 logo"
+              contain
+              src="../assets/productio.png"
+              transition="scale-transition"
+              width="40"
+            />
+            <h1 style="color: black; text-align: center; padding-top: 27%">
+              {{ role }}
+            </h1>
+          </v-carousel-item>
+        </v-carousel>
+      </div>
+    </v-col>
+    <v-col class="content">
+      <v-row class="cardrow">
+        <v-col class="card">
+          <v-card class="card" elevation="2" tile>
+            <div class="cardcontent">
+              <h1>Your information</h1>
+            </div></v-card
+          >
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
 </template>
+
+<style scoped>
+.clearancecontainer {
+  width: 100%;
+  height: 30%;
+  align-items: center;
+}
+
+.logo {
+  padding-top: 5%;
+  margin-left: 46%;
+  display: block;
+}
+
+.clearancelist {
+  height: 200px;
+  width: 80%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: white;
+}
+
+.content {
+  width: 50%;
+  font-family: "PT Serif", serif;
+}
+
+.cardcontent {
+  text-align: center;
+  padding: 5%;
+}
+
+.card {
+  height: 100%;
+}
+
+.cardrow {
+  height: 50%;
+  width: 100%;
+  padding: 4%;
+}
+</style>
 
 <script>
 export default {
-  
   name: "Profile",
+
+  data() {
+    return {
+      randomQuote: {},
+      quotes: [
+        {
+          text: "A clever person solves a problem, A wise person AVOIDS it.",
+          author: "Albert Einstein",
+        },
+        {
+          text: "Before software can be reusable it first has to be usable.",
+          author: "Ralph Johnson",
+        },
+        {
+          text: "9 women CANNOT make a baby in ONE MONTH.",
+          author: "Fred Brooks",
+        },
+        {
+          text: "Don’t let yesterday take up too much of today.",
+          author: "Will Rogers",
+        },
+        {
+          text:
+            "Failure is simply the opportunity to begin again, this time more intelligently.",
+          author: "Henry Ford",
+        },
+        {
+          text: "Today’s accomplishments were yesterday’s impossibilities.",
+          author: "Robert H. Schuller",
+        },
+      ],
+
+      roles: [],
+    };
+  },
+
+  mounted() {
+    this.randomQuote = this.quotes[
+      Math.floor(Math.random() * this.quotes.length)
+    ];
+
+    this.roles = this.$auth.user["http://Productio.net/roles"];
+
+    console.log(this.roles);
+  },
 };
 </script>
