55c0084a0265e541efc40dc02c6e6e0c4dd02ae0
SKULLCRUSHER957 <sjoerd.halmans@gmail.com>
2021-05-12 14:49:09 +0200

    basic requisitions functions done

--- productio/src/components/RequisitionsOverview.vue
+++ productio/src/components/RequisitionsOverview.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="blacktext">
-      overview
+    <v-card>
+      <div class="content">
+        <v-row>
+          <v-col cols="4"><h1>Created</h1></v-col>
+          <v-col cols="4"><h1>Type</h1></v-col>
+          <v-col cols="4"><h1>Quantity</h1></v-col>
+        </v-row>
+        <v-divider></v-divider>
+        <div v-bind:key="requisition.id" v-for="requisition in requisitions">
+          <v-row class="entry">
+            <v-col cols="4">{{ requisition.created }}</v-col>
+            <v-col cols="4">{{ requisition.name }}</v-col>
+            <v-col cols="4">{{ requisition.quantity }}</v-col>
+          </v-row>
+          <v-divider></v-divider>
+        </div>
+      </div>
+    </v-card>
   </div>
 </template>
 
@@ -30,19 +47,77 @@
 </style>
 
 <script>
+import axios from "axios";
 export default {
   name: "RequisitionsOverview",
 
   data() {
     return {
-
+      materials: [],
+      requisitions: [],
+      emptyRequisitions: [],
     };
   },
 
-
+  async mounted() {
+    await this.getMaterials();
+    await this.getEmptyRequisitions();
+    await this.fillRequisitions();
+    console.log(this.requisitions);
+    console.log(this.emptyRequisitions)
+  },
 
   methods: {
+    async getMaterials() {
+      const token = await this.$auth.getTokenSilently();
 
+      await axios
+        .get("http://localhost:3000/getmaterials", {
+          headers: {
+            Authorization: `Bearer ${token}`, // send the access token through the 'Authorization' header
+          },
+        })
+        .then((res) => {
+          this.materials = res.data;
+        });
+    },
+
+    async getEmptyRequisitions() {
+      const token = await this.$auth.getTokenSilently();
+
+      await axios
+        .get("http://localhost:3000/getallrequisitions", {
+          headers: {
+            Authorization: `Bearer ${token}`, // send the access token through the 'Authorization' header
+          },
+        })
+        .then((res) => {
+          this.emptyRequisitions = res.data;
+        });
+    },
+
+    async fillRequisitions() {
+      console.log(this.emptyRequisitions)
+      this.emptyRequisitions.forEach(async (emptyRequisition) => {
+        if (emptyRequisition.orderItemType == "Material") {
+          const filledRequisition = await this.fillMaterials(emptyRequisition);
+
+          this.requisitions.push(filledRequisition);
+        }
+      });
+    },
+
+    async fillMaterials(emptyRequisition) {
+      await this.materials.forEach((material) => {
+        if (material.id == emptyRequisition.orderedItemId) {
+          emptyRequisition.name = material.name;
+        }
+      });
+
+      emptyRequisition.quantity = emptyRequisition.quantity.toString() + " KG";
+
+      return emptyRequisition;
+    },
   },
 };
 </script>
